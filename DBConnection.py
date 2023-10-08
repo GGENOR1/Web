26a333348749840d643cd71b1fd8e2ffd4207125
GGENOR <sosokvasiliya@gmail.com>
2023-10-08 18:13:00 +0300

    1. Push

---
+++ DBConnection.py
@@ -0,0 +1,23 @@
+import motor
+from motor.motor_asyncio import AsyncIOMotorClient
+
+db_client: AsyncIOMotorClient = None
+
+
+async def connect_and_init_db():
+    global db_client
+    mongo_url = "mongodb://localhost:27017/"
+    try:
+        db_client = AsyncIOMotorClient(mongo_url)
+        print("Successful connection")
+    except Exception as ex:
+        print("Cant connection {}")
+
+
+async def close_connect():
+    global db_client
+    if db_client is None:
+        print("Connection close")
+        return
+    else:
+        db_client.close()
