6de76c22c7f6e337a2b64b9b35e23d91b91f5b37
GGENOR <sosokvasiliya@gmail.com>
2023-11-12 02:05:48 +0300

    Add memcached

--- Cache/json_serialaizer.py
+++ Cache/json_serialaizer.py
@@ -3,6 +3,7 @@ import json
 
 class JsonSerializer(object):
     def serialize(self, key, value):
+        print("Прокнуло")
         if isinstance(value, str):
             return value, 1
         if type(value).__module__ !="__builtin__":
@@ -10,6 +11,7 @@ class JsonSerializer(object):
         return json.dumps(value), 2
 
     def deserialize(self, key, value, flags):
+        print("и тут прокнула")
         if flags == 1:
             return value
         if flags == 2:
--- router.py
+++ router.py
@@ -17,21 +17,13 @@ router = APIRouter()
 message: list[Users] = []
 
 
-# def map_user(user: Any) -> Users:
-#     return Users(id=str(user["_id"]),if(creationDate=str(user['CreationDate']))is None)
-#
-#
-# def get_filter(id: str) -> dict:
-#     return {'_id': ObjectId(id)}
-
-
 # вывод всех пользователей
 @router.get("/user")
 async def get_all_users(repository: UserRepository = Depends(UserRepository.get_instance)) -> list[Users]:
     return await repository.find_all()
 
 
-# приск по имени
+# поиск по имени пользователя
 @router.get("/user/search")
 async def get_all_users(name: str, repository: UserSearchRepository = Depends(UserSearchRepository.get_instance)) -> \
         list[Users]:
@@ -58,18 +50,17 @@ async def get_by_id(user_id: str,
                     repository: UserRepository = Depends(UserRepository.get_instance),
                     memcahed_clien: HashClient = Depends(get_memcached_clinet)) -> Any:
     if not ObjectId.is_valid(user_id):
-        return JSONResponse(content = {'status' : 'BAD_REQUEST'}, status_code=status.HTTP_400_BAD_REQUEST)
+        return JSONResponse(content={'status': 'BAD_REQUEST'}, status_code=status.HTTP_400_BAD_REQUEST)
 
     user = memcahed_clien.get(user_id)
-    print(user)
+    # print(user)
     if user is not None:
         print(user)
         return user
 
-
     user = await repository.get_user_by_id(user_id)
     if user is None:
-        return JSONResponse(content = {'status' : 'NOT_FOUND'}, status_code=status.HTTP_404_NOT_FOUND)
+        return JSONResponse(content={'status': 'NOT_FOUND'}, status_code=status.HTTP_404_NOT_FOUND)
     memcahed_clien.add(user_id, user)
     return user
 
@@ -101,41 +92,47 @@ async def update_user(user_id: str,
                       search_repository: UserSearchRepository = Depends(UserSearchRepository.get_instance)
                       ) -> Any:
     if not ObjectId.is_valid(user_id):
-        return JSONResponse(content = {'status' : 'BAD_REQUEST'}, status_code=status.HTTP_400_BAD_REQUEST)
+        return JSONResponse(content={'status': 'BAD_REQUEST'}, status_code=status.HTTP_400_BAD_REQUEST)
     db_user = await repository.update(user_id, user)
     if db_user is None:
-        return JSONResponse(content = {'status' : 'NOT_FOUND'}, status_code=status.HTTP_404_NOT_FOUND)
+        return JSONResponse(content={'status': 'NOT_FOUND'}, status_code=status.HTTP_404_NOT_FOUND)
     await search_repository.update(user_id, user)
     return db_user
 
 
 ##функция для синхронизации данных из mongodb в ES
 @router.get("/message/synchronization")
-async def synchronization_Data(repository: MessageRepository = Depends(MessageRepository.get_instance),
+async def synchronization_Data(
+                               repository: MessageRepository = Depends(MessageRepository.get_instance),
                                search_repository: MessageSearchRepository
                                = Depends(MessageSearchRepository.get_instance)
                                ) -> list[Messages]:
     message = await repository.find_all()
+
+
     for mes in message:
+        mes_without_id = {key: value for key, value in mes.dict().items() if key != 'id'}
         post = await search_repository.test_find(mes.id)
         if post:
-            await search_repository.update(mes.id, mes)
+            await search_repository.update(mes.id,mes_without_id)
         else:
-            await search_repository.create(mes.id, mes)
+            await search_repository.create(mes.id, mes_without_id)
     return message
+
 
 @router.get("/users/synchronizations")
 async def synchronizations_Users(repository: UserRepository = Depends(UserRepository.get_instance),
-                        search_repository: UserSearchRepository
-                        = Depends(UserSearchRepository.get_instance)
-                        ) -> list[Users]:
+                                 search_repository: UserSearchRepository
+                                 = Depends(UserSearchRepository.get_instance)
+                                 ) -> list[Users]:
     users = await repository.find_all()
     for user in users:
         u = await search_repository.test_find(user.Id)
+        mes_without_id = {key: value for key, value in user.dict().items() if key != 'id'}
         if u:
-            await search_repository.update(user.Id, user)
+            await search_repository.update(user.Id, mes_without_id)
         else:
-            await search_repository.create(user.Id, user)
+            await search_repository.create(user.Id, mes_without_id)
     return users
 
 
@@ -151,7 +148,7 @@ async def get_by_id(message_id: str,
         return JSONResponse(content={'status': 'BAD_REQUEST'}, status_code=status.HTTP_400_BAD_REQUEST)
     db_mess = await repository.find_mess_by_id(message_id)
     if db_mess is None:
-        return JSONResponse(content={'status' : 'NOT_FOUND'}, status_code=status.HTTP_404_NOT_FOUND)
+        return JSONResponse(content={'status': 'NOT_FOUND'}, status_code=status.HTTP_404_NOT_FOUND)
     return db_mess
 
 
@@ -165,6 +162,6 @@ async def update_messages(message_id: str,
         return JSONResponse(content={'status': 'BAD_REQUEST'}, status_code=status.HTTP_400_BAD_REQUEST)
     db_mess = await repository.update_post(message_id, message)
     if db_mess is None:
-        return JSONResponse(content = {'status' : 'NOT_FOUND'}, status_code=status.HTTP_404_NOT_FOUND)
+        return JSONResponse(content={'status': 'NOT_FOUND'}, status_code=status.HTTP_404_NOT_FOUND)
     await search_repository.update(message_id, message)
     return db_mess
